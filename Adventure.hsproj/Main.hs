6f5b6a4bd7005343274ed2c8a27fd217947412ed
omersagy <omersagyemails@gmail.com>
2018-03-15 01:13:21 -0400

    main loop

---
+++ Adventure.hsproj/Main.hs
@@ -0,0 +1,11 @@
+import System.IO
+import Parser
+
+storyFile = "test.txt"
+
+main = 
+  readFile storyFile   >>= (\storyText ->
+  putStrLn "Unparsed:" >>
+  putStrLn storyText   >>
+  putStrLn "Parsed:"   >>
+  putStrLn (parse storyText))
